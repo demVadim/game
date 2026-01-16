7469970518588c4b7b6e1acbb4ca3aeb0a837cd1
demVadim <demmed85@mail.ru>
2026-01-16 16:41:15 +0800

    add rectangle

--- main.cpp
+++ main.cpp
@@ -11,11 +11,20 @@ int main()
 
     sf::CircleShape circle(64.0f);
     circle.setOrigin(circle.getGeometricCenter());
-    circle.setPosition({ width / 2.0f, height / 2.0f });
+    circle.setPosition({ width / 4.0f, height / 4.0f });
     circle.setFillColor(sf::Color::Green);
     circle.setOutlineThickness(3.0f);
     circle.setOutlineColor(sf::Color::Red);
     circle.setPointCount(3);
+
+    sf::RectangleShape rect({50.0f,80.0f});
+    rect.setOrigin(rect.getSize() / 2.0f);
+    rect.setPosition({ width / 2.0f, height / 2.0f });
+    rect.setFillColor(sf::Color::Yellow);
+    rect.setOutlineThickness(5.0f);
+    rect.setOutlineColor(sf::Color::Blue);
+    
+
 
     while(window->isOpen())
     {
@@ -36,12 +45,13 @@ int main()
             }
 
         }
-        circle.rotate(sf::degrees(1));
+        
 
         //render
         window->clear(sf::Color(255,165,0));
         //draw
         window->draw(circle);
+        window->draw(rect);
 
         window->display();
     }
