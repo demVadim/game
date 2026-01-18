eeb91d90f50f49a352dc6f4bbf7f46a4c6ef721c
demVadim <demmed85@mail.ru>
2026-01-18 18:13:31 +0800

    todo sprite

--- main.cpp
+++ main.cpp
@@ -9,21 +9,6 @@ int main()
     sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({width,height}), "Title");
     window->setFramerateLimit(60);
 
-    sf::CircleShape circle(64.0f);
-    circle.setOrigin(circle.getGeometricCenter());
-    circle.setPosition({ width / 4.0f, height / 4.0f });
-    circle.setFillColor(sf::Color::Green);
-    circle.setOutlineThickness(3.0f);
-    circle.setOutlineColor(sf::Color::Red);
-    circle.setPointCount(3);
-
-    sf::RectangleShape rect({50.0f,80.0f});
-    rect.setOrigin(rect.getSize() / 2.0f);
-    rect.setPosition({ width / 2.0f, height / 2.0f });
-    rect.setFillColor(sf::Color::Yellow);
-    rect.setOutlineThickness(5.0f);
-    rect.setOutlineColor(sf::Color::Blue);
-    
 
 
     while(window->isOpen())
@@ -45,14 +30,12 @@ int main()
             }
 
         }
-        
+
 
         //render
         window->clear(sf::Color(255,165,0));
         //draw
-        window->draw(circle);
-        window->draw(rect);
-
+      
         window->display();
     }
 
