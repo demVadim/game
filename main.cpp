d700cf9a74b8c9f9e2722455ecaeb6fb9b3dec57
demVadim <demmed85@mail.ru>
2026-01-19 16:13:51 +0800

    end sprite texture

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
-
+enum diretions {down,left,up,right};
 
 int main()
 {
@@ -10,6 +11,26 @@ int main()
     window->setFramerateLimit(60);
 
     //todo sprite
+    sf::Texture texture;
+
+    if (!texture.loadFromFile("sprite/BODY_skeleton.png")){
+
+        std::cerr<<"error load file skeleton"<< std::endl;
+        return -1;         
+    }
+
+    sf::Sprite sprite(texture);
+
+    sf::IntRect dir[4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        dir[i] = sf::IntRect( { {0 , 64*i},{64,64} } );
+    }
+    
+    sprite.setTextureRect(dir[up]);
+    sprite.setOrigin({64,64});
+    sprite.setPosition({width/2.0f,height/2.0f});
 
     while(window->isOpen())
     {
@@ -31,10 +52,29 @@ int main()
 
         }
 
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::S)){
+
+            sprite.move({0.0f,1.0f});
+            sprite.setTextureRect(dir[up]);            
+
+        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::W)){
+
+            sprite.move({0.0f,-1.0f});
+            sprite.setTextureRect(dir[down]);
+        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::D)){
+
+            sprite.move({1.0f,0.0f});
+            sprite.setTextureRect(dir[right]);
+        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::A)){
+
+            sprite.move({-1.0f,0.0f});
+            sprite.setTextureRect(dir[left]);
+        }
         //render
         window->clear(sf::Color(255,165,0));
         //draw
-      
+        window->draw(sprite);
+
         window->display();
     }
 
